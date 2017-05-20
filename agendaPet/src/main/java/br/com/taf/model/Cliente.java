617532c1b8c9492ddc71937336e63ac502c18250
ClepsonSantos <clepsonsantos9@gmail.com>
2017-05-19 21:03:39 -0300

    👽 Criada a classe Cliente,,

---
+++ agendaPet/src/main/java/br/com/taf/model/Cliente.java
@@ -0,0 +1,116 @@
+package br.com.taf.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //entidade
+public class Cliente {
+
+	@Id //chave primaria
+	@GeneratedValue //autoincremente
+	private Long id;
+	@Column(length = 250, nullable = false)//quantidade maxima de caractere e valor não pode ser nulu
+	private String nome;
+	@Column(length = 20)
+	private String cpf;
+	@Column(length = 20)
+	private String rg;
+	@Column(length = 250)
+	private String endereco;
+	@Column(length = 20)
+	private String celular;
+	@Column(length = 20)
+	private String telefone;
+
+	public Cliente() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public String toString() {
+		return getNome();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}

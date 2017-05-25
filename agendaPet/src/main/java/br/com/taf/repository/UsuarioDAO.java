5e75a059a4874fce28bdac7056b1ab2dad1beec2
JrCezzar <jrcezzar86@gmail.com>
2017-05-25 20:32:52 -0300

    Criação da classe UsuarioDAO.

---
+++ agendaPet/src/main/java/br/com/taf/repository/UsuarioDAO.java
@@ -0,0 +1,59 @@
+package br.com.taf.repository;
+
+import javax.swing.JOptionPane;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.taf.model.Usuario;
+
+public class UsuarioDAO extends GenericDAO<Usuario> {
+
+	public UsuarioDAO() {
+		super(Usuario.class);
+	} 
+
+	// Alterado
+	public boolean salvar(Usuario u) {
+		if (u.getId() == null) {
+			// Se salvar mostra essa mensagem
+			JOptionPane.showMessageDialog(null, "Salvou!");
+			return super.salvar(u);
+		} else {
+			// Se editar mostra essa mensagem
+			JOptionPane.showMessageDialog(null, "Editou!");
+			return super.editar(u);
+		}
+	}
+
+	public Usuario buscaLogin(String usuario, String senha) throws Exception {
+		Session session = null;
+		try {//captura o erro
+			session = getSession().openSession();
+			Criteria criteria = session.createCriteria(Usuario.class);
+			criteria.add(org.hibernate.criterion.Restrictions.eq("login", usuario));
+			criteria.add(org.hibernate.criterion.Restrictions.eq("senha", senha));
+			return (Usuario) criteria.uniqueResult();
+		} catch (Exception e) { //trata a excessão
+			System.out.println(e);
+			throw e;
+		} finally {//finamliza a sessão
+			if (session != null && session.isOpen()) {
+				session.close();
+			}
+		}
+	}
+
+	private Usuario verificaSeDadosIguais(Usuario usuario) {//verifica se se os dados são iguais
+		Session session = getSession().openSession();
+		return (Usuario) session.createCriteria(Usuario.class).add(Restrictions.eq("login", usuario.getLogin()))
+				.add(Restrictions.eq("senha", usuario.getSenha())).uniqueResult();
+	}
+
+	public boolean existeUsuarioSemelhante(Usuario usuario) { //verifica se exite usuario iguais
+		Usuario usuarioSemelhante = this.verificaSeDadosIguais(usuario);
+		return usuarioSemelhante != null && !usuarioSemelhante.equals(usuario);
+	}
+
+}
